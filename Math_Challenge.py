0f2317ca61211937d7d8ef0fc580c2f7566db170
AsifShaikh-ui <sasif9226@gmail.com>
2026-01-16 23:21:19 +0530

    Update the challenge with one additon of the operator make it more difficult to solve problems

--- Math_Challenge.py
+++ Math_Challenge.py
@@ -2,17 +2,22 @@ import time
 import random
 
 def math_challenge():
-    operator = ["+","-","*"]
+    operator = ["+","-","*","/"]
     start_time = time.time()
     count = 0
+    print("You have 1 min! \nGood Luck")
     while time.time() - start_time <=60:
-        print("You have 1 min to solve")
-        fisrt_vaiable = random.randint(1,50)
-        second_variable = random.randint(1,50)
+        
+        fisrt_vaiable = random.randint(1,20)
+        second_variable = random.randint(1,20)
         ops = random.choice(operator)
-        a_op_b = "{} {} {}".format(fisrt_vaiable, ops, second_variable)
-        ans = int(eval(a_op_b))
-
+        if ops == "/":
+            b_op_a = "{} {} {}".format(second_variable, ops, fisrt_vaiable)
+            ans = int(eval(b_op_a))
+        else:    
+            a_op_b = "{} {} {}".format(fisrt_vaiable, ops, second_variable)
+            ans = int(eval(a_op_b))
+        print(f"Remaining {int(60 - (time.time() - start_time))} seconds")
         user_answer = int(input(f"{fisrt_vaiable} {ops} {second_variable} : "))
 
         if user_answer == ans:

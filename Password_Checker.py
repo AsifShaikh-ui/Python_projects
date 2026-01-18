3f11d5e65544a4302968ba380caaeea16c28eedf
AsifShaikh-ui <sasif9226@gmail.com>
2026-01-18 23:03:30 +0530

    Generate a program that checks the password and user_name for fisrt level security

---
+++ Password_Checker.py
@@ -0,0 +1,12 @@
+def log_in(user_name, password):
+    user = input("Enter your user_name : ")
+    pass_word = int(input("Enter the password: "))
+
+    if user == user_name and  pass_word == password:
+        print("Log In")
+    else:
+        print("User_name OR paasword Incorrect! ")
+
+user_name = "xyzalpha"
+password = 25807436
+log_in(user_name, password)

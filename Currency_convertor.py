6cf843bbe09b881b7261d7673db51606d7e23404
AsifShaikh-ui <sasif9226@gmail.com>
2026-01-21 22:59:22 +0530

    Make some small changes for better exprerince

--- Currency_convertor.py
+++ Currency_convertor.py
@@ -5,6 +5,7 @@ def dol_to_ind(currency):
     print(f"Your wallet after convertion : {round((currency*82.3),2)}")
 
 def main():
+    print("\n-------Currency Converter--------")
     user = input("Enter the amount type Indian/Dollar (I/D): ").strip().lower()
     if user == "i":
         currency = int(input("Enter the amount: "))

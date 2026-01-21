59924881b3d4531de7f26b6f25df7acee4ed9afe
AsifShaikh-ui <sasif9226@gmail.com>
2026-01-21 22:57:12 +0530

    Done with a logics use functions for better structured

---
+++ Currency_convertor.py
@@ -0,0 +1,19 @@
+def ind_to_dol(currency):
+    print(f"Your wallet after convertion : {round(currency/82.3,2)}")
+
+def dol_to_ind(currency):
+    print(f"Your wallet after convertion : {round((currency*82.3),2)}")
+
+def main():
+    user = input("Enter the amount type Indian/Dollar (I/D): ").strip().lower()
+    if user == "i":
+        currency = int(input("Enter the amount: "))
+        ind_to_dol(currency)
+    elif user == "d":
+        currency = int(input("Enter the amount: "))
+        dol_to_ind(currency)
+    else:
+        print("Enter Valid Option")
+
+if __name__ == "__main__":
+    main()

6cbfaddcaf6d22815991730c34d6943a5c12fe46
AsifShaikh-ui <sasif9226@gmail.com>
2026-01-31 22:51:26 +0530

    Make the update function to update the task status in database

--- To-Do_database/database.py
+++ To-Do_database/database.py
@@ -59,3 +59,17 @@ def delete_task(task_id):
 
     return deleted
 
+def mark_done(task_id):
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+       UPDATE task SET status = ? WHERE id = ? 
+""", ("Done", task_id))
+    
+    conn.commit()
+    updated = cursor.rowcount
+    conn.close()
+
+    return updated

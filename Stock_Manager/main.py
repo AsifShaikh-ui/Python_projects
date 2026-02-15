ab38cbd777954d7c31c073a7159cec03bb09b002
AsifShaikh-ui <sasif9226@gmail.com>
2026-02-15 20:47:19 +0530

    Implement the sample main layer to check the flow of operations

--- Stock_Manager/main.py
+++ Stock_Manager/main.py
@@ -1,6 +1,10 @@
 from database import(
     create_table,
-    add_product
+    add_product,
+    get_product_by_id,
+    get_all_products,
+    update_product_quantity,
+    delete_product
 )
 
 create_table()
@@ -9,6 +13,27 @@ name = input("Enter your full name : ")
 quantity = int(input("Enter the quantity of stock : "))
 price = int(input("Enter the total price of stock : "))
 low_stock = int(input("Enter the threshold quantity of stock : "))
-created_at = input("Enter the date of buy : ")
 
-add_product(name, quantity, price, low_stock, created_at)
+add_product(name, quantity, price, low_stock)
+
+id = int(input("Enter the id of the stock: "))
+
+value = get_product_by_id(id)
+print(value)
+
+get_list = get_all_products()
+print(get_list)
+
+id_1 = int(input("Enter the id of the stock: "))
+new_quantity = int(input("Enter the quantity of stock : "))
+update_product_quantity(id_1, new_quantity)
+
+get_list_1 = get_all_products()
+print(get_list_1)
+
+id_2 = int(input("Enter the id of the stock: "))
+delete_product(id_2)
+
+
+
+

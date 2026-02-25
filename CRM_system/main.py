51b17b26e77fdb97f2dfeea1877f7e55ffbe0257
AsifShaikh-ui <sasif9226@gmail.com>
2026-02-25 23:43:19 +0530

    Add CRM command line interface with user validations

---
+++ CRM_system/main.py
@@ -0,0 +1,80 @@
+from database import (
+    create_table
+)
+
+from service import (
+    create_client,
+    add_interaction,
+    update_client_status
+)
+
+def main():
+
+    create_table()
+
+    while True:
+
+        print("---------------- CRM System ----------------")
+        print("""
+    1️⃣. Add client
+    2️⃣. Add interaction
+    3️⃣. Update Status
+    4️⃣. View Records By Status
+    5️⃣. Exit
+    """)
+        
+        choice  = input("Choose from menu : ")
+
+        if choice == "1":
+
+            name  = input("Enter the name of client: ")
+            phone = input("Clients phone_no : ")
+            email = input("Clients email : ")
+
+            try:
+                create_client(name, phone, email)
+                print("Client added successfully✔️")
+            except ValueError as e:
+                print(f"Error : {e}")
+            except Exception as e:
+                print(f"Error : {e}")
+
+
+        elif choice == "2":
+            
+            client_id = int(input("Enter the client_id : "))
+            type = input("Enter the way of interaction: ")
+            note = input("Leave some note : ")
+
+            try:
+                add_interaction(client_id, type, note)
+                print("Interaction Record saved successfully✔️")
+            except ValueError as e:
+                print(f"Error : {e}")
+            except Exception as e:
+                print(f"Error : {e}")
+
+
+        elif choice == "3":
+            
+            client_id = int(input("Enter the client_id : "))
+            new_status = input("Enter the updated status :")
+
+            try:
+                update_status = update_client_status(client_id, new_status)
+                print(f"Client_id : {update_status["client_id"]}")
+                print(f"New status : {update_status["Update_status"]}")
+            except ValueError as e:
+                print(f"Error : {e}")
+            except Exception as e:
+                print(f"Error : {e}")
+
+
+        elif choice == "4":
+            pass
+
+        elif choice == "5":
+            print("Good-Bye.....")
+            break
+        else:
+            print("Choose from menu only")
